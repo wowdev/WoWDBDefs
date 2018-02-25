200797f2fc5737229c68632181564dcf9fe53cac
Martin Benjamins <marlamin@marlamin.com>
2018-02-25 16:35:01 +0100

    Validator whole directory

--- code/C#/DBDefsValidator/Program.cs
+++ code/C#/DBDefsValidator/Program.cs
@@ -1,14 +1,23 @@
 ﻿using DBDefsLib;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using static DBDefsLib.Structs;
 
 namespace DBDefsTest
 {
     class Program
     {
+        public static Dictionary<string, DBDefinition> definitionCache = new Dictionary<string, DBDefinition>();
+
         static void Main(string[] args)
         {
-            var reader = new DBDReader();
-            var definition = reader.Read("../../../definitions/Map.dbd");
+            foreach (var file in Directory.GetFiles("../../../definitions/"))
+            {
+                var reader = new DBDReader();
+                Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+                definitionCache.Add(Path.GetFileNameWithoutExtension(file), reader.Read(file));
+            }
             Console.WriteLine("Done, press enter to exit");
             Console.ReadLine();
         }
