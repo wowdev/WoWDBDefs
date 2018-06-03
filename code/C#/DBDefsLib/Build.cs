ab22685f2c7216b76b523c5832e80f2e840a1fef
Martin Benjamins <marlamin@marlamin.com>
2018-06-03 12:30:30 +0200

    code: C#: Make Build comparable

--- code/C#/DBDefsLib/Build.cs
+++ code/C#/DBDefsLib/Build.cs
@@ -2,7 +2,7 @@
 
 namespace DBDefsLib
 {
-    public class Build
+    public class Build : IComparable
     {
         public short expansion;
         public short major;
@@ -31,16 +31,49 @@ namespace DBDefsLib
 
         public override int GetHashCode()
         {
-            if (build       > 0xFFFFF) throw new Exception("Build too large for fake hash code");
-            if (minor       > 0xF) throw new Exception("Minor too large for fake hash code");
-            if (major       > 0xF) throw new Exception("Major too large for fake hash code");
-            if (expansion   > 0xF) throw new Exception("Expansion too large for fake hash code");
+            if (build > 0xFFFFF) throw new Exception("Build too large for fake hash code");
+            if (minor > 0xF) throw new Exception("Minor too large for fake hash code");
+            if (major > 0xF) throw new Exception("Major too large for fake hash code");
+            if (expansion > 0xF) throw new Exception("Expansion too large for fake hash code");
             return (int)((uint)expansion << 28 | (uint)major << 24 | (uint)minor << 20 | build);
         }
 
         public bool Equals(Build build)
         {
             return build != null && build.expansion == expansion && build.major == major && build.minor == minor && build.build == this.build;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            if (obj is Build otherBuild)
+            {
+                if (expansion != otherBuild.expansion)
+                {
+                    return expansion.CompareTo(otherBuild.expansion);
+                }
+                else if (major != otherBuild.major)
+                {
+                    return major.CompareTo(otherBuild.major);
+                }
+                else if (minor != otherBuild.minor)
+                {
+                    return minor.CompareTo(otherBuild.minor);
+                }
+                else if (build != otherBuild.build)
+                {
+                    return build.CompareTo(otherBuild.build);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Object is not a valid build!");
+            }
         }
     }
 }
