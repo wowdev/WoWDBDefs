4c2c546cd43ace172d1d98638862ad57dedfe6a3
Martin Benjamins <marlamin@marlamin.com>
2018-06-10 22:29:36 +0200

    code: C#: Add 7.3.5 PTR pattern

--- code/C#/DBDefsDumper/PatternBuilder.cs
+++ code/C#/DBDefsDumper/PatternBuilder.cs
@@ -72,6 +72,36 @@ namespace DBDefsDumper
             );
 
             patterns.Add(
+            new Pattern("7.3.5-ptr")
+                .Pointer(Name.DB_NAME)
+                .FieldReference(Name.NUM_FIELD_IN_FILE)
+                .RecordSize(Name.RECORD_SIZE)
+                .FieldReference(Name.NUM_FIELD)
+                .OptionalFieldReference(Name.ID_COLUMN)
+                .Boolean(Name.SPARSE_TABLE)
+                .Pointer(Name.FIELD_OFFSETS)
+                .Pointer(Name.FIELD_SIZES)
+                .Pointer(Name.FIELD_TYPES)
+                .Pointer(Name.FIELD_SIZES_IN_FILE)
+                .Pointer(Name.FIELD_TYPES_IN_FILE)
+                .Pointer(Name.FIELD_FLAGS_IN_FILE)
+                .Uint8(Name.FLAGS_58_21)
+                .Hash(Name.TABLE_HASH)
+                .Append("00", "00", "00", "00")
+                .Hash(Name.LAYOUT_HASH)
+                .Uint8(Name.FLAGS_68_421)
+                .FieldReference(Name.FIELD_NUM_IDX_INT)
+                .FieldReference(Name.FIELD_NUM_IDX_STRING)
+                .OptionalPointer(Name.FIELD_IDX_INT)
+                .OptionalPointer(Name.FIELD_IDX_STRING)
+                .Boolean(Name.UNK88)
+                .OptionalFieldReference(Name.FIELD_RELATION)
+                .OptionalFieldReference(Name.FIELD_RELATION_IN_FILE)
+                .OptionalPointer(Name.SORT_FUNC)
+                .Boolean(Name.UNKC0)
+            );
+
+            patterns.Add(
                 new Pattern("7.{2.5,3.{0,2}}-release") // note: also matches release-7.3.5 even though different struct
                 .Pointer(Name.DB_NAME)
                 .FieldReference(Name.NUM_FIELD_IN_FILE)
