057afd2d4188966fabdb2f5fc156ae6f69917345
barncastle <spearman.tom@gmail.com>
2018-07-15 19:44:50 +0100

    code: C#: contains fix

--- code/C#/DBDefsLib/BuildRange.cs
+++ code/C#/DBDefsLib/BuildRange.cs
@@ -75,7 +75,7 @@ namespace DBDefsLib
             return 
                 build.expansion >= minBuild.expansion && build.expansion <= maxBuild.expansion && 
                 build.major >= minBuild.major && build.major <= maxBuild.major && 
-                build.minor >= minBuild.minor && build.major <= maxBuild.minor && 
+                build.minor >= minBuild.minor && build.minor <= maxBuild.minor && 
                 build.build >= minBuild.build && build.build <= maxBuild.build;
         }
     }
