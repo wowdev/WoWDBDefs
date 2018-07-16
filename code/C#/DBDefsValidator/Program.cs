91acfd3ae1df013559f6c80ab5c794a3dd423705
barncastle <spearman.tom@gmail.com>
2018-07-16 14:04:52 +0100

    code: c#: verified unknown field/padding check

--- code/C#/DBDefsValidator/Program.cs
+++ code/C#/DBDefsValidator/Program.cs
@@ -6,82 +6,102 @@ using static DBDefsLib.Structs;
 
 namespace DBDefsTest
 {
-    class Program
-    {
-        public static Dictionary<string, DBDefinition> definitionCache = new Dictionary<string, DBDefinition>();
+	class Program
+	{
+		public static Dictionary<string, DBDefinition> definitionCache = new Dictionary<string, DBDefinition>();
 
-        static void Main(string[] args)
-        {
-            if(args.Length < 1)
-            {
-                Console.WriteLine("Usage: <definitionsdir>");
-                Environment.Exit(1);
-            }
+		static void Main(string[] args)
+		{
+			if(args.Length < 1)
+			{
+				Console.WriteLine("Usage: <definitionsdir>");
+				Environment.Exit(1);
+			}
 
-            var definitionDir = args[0];
+			var definitionDir = args[0];
 
-            var rewrite = false;
+			var rewrite = false;
 
-            if (args.Length == 2)
-            {
-                if (args[1] == "true")
-                {
-                    rewrite = true;
-                }
-            }
+			if (args.Length == 2)
+			{
+				if (args[1] == "true")
+				{
+					rewrite = true;
+				}
+			}
 
-            foreach (var file in Directory.GetFiles(definitionDir))
-            {
-                var dbName = Path.GetFileNameWithoutExtension(file);
+			foreach (var file in Directory.GetFiles(definitionDir))
+			{
+				var dbName = Path.GetFileNameWithoutExtension(file);
 
-                var reader = new DBDReader();
-                definitionCache.Add(dbName, reader.Read(file, true));
+				var reader = new DBDReader();
+				definitionCache.Add(dbName, reader.Read(file, true));
 
-                Console.WriteLine("Read " + definitionCache[dbName].versionDefinitions.Length + " versions and " + definitionCache[dbName].columnDefinitions.Count + " columns for " + dbName);
-                if (rewrite)
-                {
-                    var writer = new DBDWriter();
-                    writer.Save(definitionCache[dbName], Path.Combine(definitionDir, dbName + ".dbd"));
-                }
-            }
+				Console.WriteLine("Read " + definitionCache[dbName].versionDefinitions.Length + " versions and " + definitionCache[dbName].columnDefinitions.Count + " columns for " + dbName);
+				if (rewrite)
+				{
+					var writer = new DBDWriter();
+					writer.Save(definitionCache[dbName], Path.Combine(definitionDir, dbName + ".dbd"));
+				}
+			}
 
-            Console.WriteLine("Read " + definitionCache.Count + " database definitions!");
+			Console.WriteLine("Read " + definitionCache.Count + " database definitions!");
 
-            var errorEncountered = false;
+			var errorEncountered = false;
 
-            var foreignKeys = 0;
-            foreach(var definition in definitionCache)
-            {
-                foreach(var columnDefinition in definition.Value.columnDefinitions)
-                {
-                    if (!string.IsNullOrEmpty(columnDefinition.Value.foreignTable) || !string.IsNullOrEmpty(columnDefinition.Value.foreignColumn))
-                    {
-                        if(definitionCache.ContainsKey(columnDefinition.Value.foreignTable) && definitionCache[columnDefinition.Value.foreignTable].columnDefinitions.ContainsKey(columnDefinition.Value.foreignColumn))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            //Console.WriteLine(definition.Key + "." + columnDefinition.Key + " has a foreign key to " + columnDefinition.Value.foreignTable + "." + columnDefinition.Value.foreignColumn);
-                        }
-                        else
-                        {
-                            errorEncountered = true;
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine(definition.Key + "." + columnDefinition.Key + " has a foreign key to " + columnDefinition.Value.foreignTable + "." + columnDefinition.Value.foreignColumn + " WHICH DOES NOT EXIST!");
-                        }
+			var foreignKeys = 0;
+			foreach(var definition in definitionCache)
+			{
+				foreach(var columnDefinition in definition.Value.columnDefinitions)
+				{
+					if (!string.IsNullOrEmpty(columnDefinition.Value.foreignTable) || !string.IsNullOrEmpty(columnDefinition.Value.foreignColumn))
+					{
+						if(definitionCache.ContainsKey(columnDefinition.Value.foreignTable) && definitionCache[columnDefinition.Value.foreignTable].columnDefinitions.ContainsKey(columnDefinition.Value.foreignColumn))
+						{
+							Console.ForegroundColor = ConsoleColor.Green;
+							//Console.WriteLine(definition.Key + "." + columnDefinition.Key + " has a foreign key to " + columnDefinition.Value.foreignTable + "." + columnDefinition.Value.foreignColumn);
+						}
+						else
+						{
+							errorEncountered = true;
+							Console.ForegroundColor = ConsoleColor.Red;
+							Console.WriteLine(definition.Key + "." + columnDefinition.Key + " has a foreign key to " + columnDefinition.Value.foreignTable + "." + columnDefinition.Value.foreignColumn + " WHICH DOES NOT EXIST!");
+						}
 
-                        foreignKeys++;
+						foreignKeys++;
 
-                        Console.ResetColor();
-                    }
-                }
-            }
+						Console.ResetColor();
+					}
+				}
+			}
+			Console.WriteLine("Checked " + foreignKeys + " foreign keys!");
 
-            Console.WriteLine("Checked " + foreignKeys + " foreign keys!");
-            Console.WriteLine("Done");
+			int unverfiedFields = 0;
+			foreach (var definition in definitionCache)
+			{
+				foreach (var columnDefinition in definition.Value.columnDefinitions)
+				{
+					string name = columnDefinition.Key.ToLower();
+					if (name.StartsWith("padding_") || name.StartsWith("field_"))
+					{
+						unverfiedFields++;
 
-            if (errorEncountered)
-            {
-                Environment.Exit(1);
-            }
-        }
-    }
+						if (columnDefinition.Value.verified)
+						{
+							Console.ForegroundColor = ConsoleColor.Yellow;
+							Console.WriteLine(definition.Key + "." + columnDefinition.Key + " is verified but is an unknown field/padding.");
+							Console.ResetColor();
+						}
+					}
+				}
+			}
+			Console.WriteLine("Checked " + unverfiedFields + " unknown/padding fields!");
+			Console.WriteLine("Done");
+
+			if (errorEncountered)
+			{
+				Environment.Exit(1);
+			}
+		}
+	}
 }
