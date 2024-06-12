a0235e36a7d1acbd42bf5feb266a07b805581d96
Martin Benjamins <ictmartin@gmail.com>
2024-06-12 21:06:34 +0200

    Update PatternBuilder.cs

--- code/C#/DBDefsDumper/PatternBuilder.cs
+++ code/C#/DBDefsDumper/PatternBuilder.cs
@@ -77,7 +77,7 @@ namespace DBDefsDumper
             );
             
             patterns.Add(
-            new Pattern("10.1.0", new List<string> { "10.1.0", "10.1.5", "1.14.4", "1.15.0", "3.4.2", "10.1.7", "10.2.0", "3.4.3", "10.2.5", "4.4.0", "1.15.1", "10.2.7", "10.2.6", "1.15.2"}, 48480, 0)
+            new Pattern("10.1.0", new List<string> { "10.1.0", "10.1.5", "1.14.4", "1.15.0", "3.4.2", "10.1.7", "10.2.0", "3.4.3", "10.2.5", "4.4.0", "1.15.1", "10.2.7", "10.2.6", "1.15.2", "1.15.3" }, 48480, 0)
                .Pointer(Name.DB_NAME)
                .FileDataID(Name.FDID)
                .FieldReference(Name.NUM_FIELD_IN_FILE)
