837f1a4f6ebf2025b51052e59f0092cf2ac49402
Marlamin <ictmartin@gmail.com>
2026-05-18 09:26:15 +0200

    Add preset build ranges (for meta files)

--- code/C#/DBDefsLib/BuildRange.cs
+++ code/C#/DBDefsLib/BuildRange.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace DBDefsLib
 {
     public class BuildRange : IComparable
     {
+        private static readonly ReadOnlyDictionary<string, (Build minBuild, Build maxBuild)> presetRanges = new(new Dictionary<string, (Build minBuild, Build maxBuild)>
+        {
+            { "Pre", (new Build(0, 3, 4, 2953), new Build(0, 12, 0, 3988))  },
+            { "Vanilla", (new Build(1, 0, 0, 3980), new Build(1, 12, 3, 6141)) },
+            { "TBC", (new Build(2, 0, 0, 5610), new Build(2, 4, 3, 8606)) },
+            { "WotLK", (new Build(3, 0, 0, 8120), new Build(3, 3, 5, 12340)) },
+            { "Cata", (new Build(4, 0, 0, 11927), new Build(4, 3, 4, 15595)) },
+            { "MoP", (new Build(5, 0, 1, 15464), new Build(5, 4, 8, 18414)) },
+            { "WoD", (new Build(6, 0, 1, 18125), new Build(6, 2, 4, 21742)) },
+            { "Legion", (new Build(7, 0, 1, 20740), new Build(7, 3, 5, 26972)) },
+            { "BfA", (new Build(8, 0, 1, 25703), new Build(8, 3, 7, 35662)) },
+            { "SL", (new Build(9, 0, 1, 33411), new Build(9, 2, 7, 45745)) },
+            { "DF", (new Build(10, 0, 0, 43342), new Build(10, 2, 7, 55664)) },
+            { "TWW", (new Build(11, 0, 0, 54311), new Build(11, 2, 7, 65299)) },
+            // Midnight
+            // TLT
+        });
+
         public Build minBuild;
         public Build maxBuild;
+        private bool InitializedAsPreset = false;
 
         /// <summary>
         /// Serialization requirement.
@@ -23,6 +44,14 @@ namespace DBDefsLib
 
         public BuildRange(string buildRange)
         {
+            if (presetRanges.TryGetValue(buildRange, out var presetRange))
+            {
+                this.minBuild = presetRange.minBuild;
+                this.maxBuild = presetRange.maxBuild;
+                InitializedAsPreset = true;
+                return;
+            }
+
             var split = buildRange.Split('-');
             this.minBuild = new Build(split[0]);
             this.maxBuild = new Build(split[1]);
@@ -33,6 +62,13 @@ namespace DBDefsLib
 
         public override string ToString()
         {
+            if (InitializedAsPreset)
+            {
+                foreach(var preset in presetRanges)
+                    if(preset.Value.minBuild == minBuild && preset.Value.maxBuild == maxBuild)
+                        return preset.Key;
+            }
+
             return minBuild.ToString() + "-" + maxBuild.ToString();
         }
 
@@ -169,6 +205,18 @@ namespace DBDefsLib
         public static bool TryParse(string value, out BuildRange result)
         {
             result = null;
+
+            if (presetRanges.TryGetValue(value, out var presetRange))
+            {
+                result = new BuildRange()
+                {
+                    minBuild = presetRange.minBuild,
+                    maxBuild = presetRange.maxBuild,
+                    InitializedAsPreset = true
+                };
+
+                return true;
+            }
 
             var parts = value.Split('-');
             if (parts.Length != 2)
